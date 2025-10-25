bf9af1ce18dcda58434f8ad0e28065966225dce6
DLearner6975 <26613413+DLearner6975@users.noreply.github.com>
2025-10-25 18:29:34 -0500

    fix type issues

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./app/layout/styles.css";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
 import "@fontsource-variable/nunito";
 import "react-toastify/dist/ReactToastify.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
