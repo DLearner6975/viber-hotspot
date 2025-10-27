70ce212e69982c03d2e34c7b0bbabcc70b11b7a5
DLearner6975 <26613413+DLearner6975@users.noreply.github.com>
2025-10-27 13:53:15 -0500

    adjust form

--- client/src/features/activities/form/ActivityForm.tsx
+++ client/src/features/activities/form/ActivityForm.tsx
@@ -1,4 +1,16 @@
-import { Box, Button, Paper, Typography } from "@mui/material";
+import {
+    Box,
+    Button,
+    Card,
+    CardContent,
+    CardHeader,
+    CardMedia,
+    Grid,
+    InputLabel,
+    Paper,
+    Typography,
+    type SxProps,
+} from "@mui/material";
 import { useActivities } from "../../../lib/hooks/useActivities";
 import { useNavigate, useParams } from "react-router";
 import { useForm } from "react-hook-form";
@@ -11,6 +23,27 @@ import { categoryOptions } from "./categoryOptions";
 import SelectInput from "../../../app/shared/components/SelectInput";
 import DateTimeInput from "../../../app/shared/components/DateTimeInput";
 import LocationInput from "../../../app/shared/components/LocationInput";
+import type { Theme } from "@mui/material/styles";
+
+const cardTitleWhite: SxProps<Theme> = {
+    color: "white",
+    marginTop: "0px",
+    minHeight: "auto",
+    fontWeight: "300",
+    // fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
+    marginBottom: "3px",
+    textDecoration: "none",
+};
+const cardCategoryWhite: SxProps<Theme> = {
+    color: "white",
+    margin: "0 0 3px",
+    padding: "0",
+    fontSize: "14px",
+    textTransform: "uppercase",
+    fontWeight: "500",
+    // fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
+    letterSpacing: "0",
+};
 
 export default function ActivityForm() {
     const { reset, handleSubmit, control } = useForm<ActivitySchema>({
@@ -57,55 +90,88 @@ export default function ActivityForm() {
     if (isLoadingActivity) return <Typography>Loading...</Typography>;
 
     return (
-        <Paper sx={{ borderRadius: 3, padding: 3 }}>
-            <Typography variant="h5" gutterBottom color="primary">
-                {activity ? "Edit Activity" : "Create Activity"}
-            </Typography>
-            <Box
-                component="form"
-                onSubmit={handleSubmit(onSubmit)}
-                display="flex"
-                flexDirection="column"
-                gap={3}
-            >
-                <TextInput label="Title" control={control} name="title" />
-                <TextInput
-                    label="Description"
-                    control={control}
-                    name="description"
-                    multiline
-                    rows={3}
-                />
-                <Box sx={{ display: "flex", gap: 3 }}>
-                    <SelectInput
-                        items={categoryOptions}
-                        label="Category"
+        <Card>
+            <CardHeader
+                sx={{ bgcolor: "primary.main" }}
+                title={activity ? "Edit Activity" : "Create Activity"}
+                subheader={
+                    activity ? "Edit your activity" : "Create a new activity"
+                }
+                slotProps={{
+                    title: {
+                        variant: "h4",
+                        sx: cardTitleWhite,
+                    },
+                    subheader: {
+                        sx: cardCategoryWhite,
+                    },
+                }}
+            />
+            <CardContent>
+                <Box
+                    component="form"
+                    onSubmit={handleSubmit(onSubmit)}
+                    display="flex"
+                    flexDirection="column"
+                    gap={3}
+                >
+                    <TextInput label="Title" control={control} name="title" />
+                    <TextInput
+                        label="Description"
                         control={control}
-                        name="category"
+                        name="description"
+                        multiline
+                        rows={3}
                     />
-                    <DateTimeInput label="Date" control={control} name="date" />
-                </Box>
-                <LocationInput
-                    label="Enter the location"
-                    control={control}
-                    name="location"
-                />
-                <Box display="flex" justifyContent="end" gap={3}>
-                    <Button onClick={() => navigate(-1)} color="inherit">
-                        Cancel
-                    </Button>
-                    <Button
-                        type="submit"
-                        variant="contained"
-                        color="success"
-                        disabled={
-                            updateActivity.isPending || createActivity.isPending
-                        }
+                    <Box
+                        sx={{
+                            display: "flex",
+                            gap: 3,
+                            flexDirection: { xs: "column", md: "row" },
+                        }}
                     >
-                        Submit
-                    </Button>
+                        <SelectInput
+                            items={categoryOptions}
+                            label="Category"
+                            control={control}
+                            name="category"
+                        />
+                        <DateTimeInput
+                            label="Date"
+                            control={control}
+                            name="date"
+                        />
+                    </Box>
+                    <LocationInput
+                        label="Enter the location"
+                        control={control}
+                        name="location"
+                    />
+                    <Box display="flex" justifyContent="end" gap={3}>
+                        <Button onClick={() => navigate(-1)} color="inherit">
+                            Cancel
+                        </Button>
+                        <Button
+                            type="submit"
+                            variant="contained"
+                            color="primary"
+                            sx={{
+                                color: "white",
+                                backgroundColor: "primary.main",
+                                "&:hover": {
+                                    backgroundColor: "primary.dark",
+                                },
+                            }}
+                            disabled={
+                                updateActivity.isPending ||
+                                createActivity.isPending
+                            }
+                        >
+                            Submit
+                        </Button>
+                    </Box>
                 </Box>
-            </Box>
-        </Paper>
+            </CardContent>
+        </Card>
     );
 }
